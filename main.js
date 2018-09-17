3f9145fbdeb27e77e6cb9c9db728418c70919e1f
Shin Narita <triplfjump1417@gmail.com>
2018-09-17 15:55:06 +0900

    rootにelectronのwindow作成追加

---
+++ main.js
@@ -0,0 +1,30 @@
+import {
+  app,
+  BrowserWindow
+} from 'electron';
+
+let mainWindow;
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    title: app.getName()
+  });
+
+  mainWindow.loadFile('index.html')
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  })
+};
+
+app.on('window-all-closed', () => {
+  if (process.platform != 'darwin') app.quit();
+});
+
+app.on('ready', () => createWindow);
+
+app.on('active', () => {
+  if (mainWindow === null) createWindow;
+});
